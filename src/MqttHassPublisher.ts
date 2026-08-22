af57fb94f604ff572341b8eca36637986735a7d7
Doug Lampe <lampedoug@yahoo.com>
2026-08-22 13:33:32 -0400

    feat: add payload file generation

--- src/MqttHassPublisher.ts
+++ src/MqttHassPublisher.ts
@@ -18,6 +18,7 @@ export interface HomeAssistantConfig {
   prefix: string;
   configFile: string;
   shortNames: boolean;
+  payloadFile: string;
 }
 
 export interface MqttManagerOptions {
@@ -49,6 +50,8 @@ export class MqttHassPublisher {
   private availabilityTopic: string;
   private stateTopic: string;
   private changeTopic: string;
+  private payloadFile: string;
+  public payloads: Array<{ topic: string, payload: string }> = [];
 
   constructor(options: MqttManagerOptions) {
     this.receiver = options.receiver;
@@ -58,6 +61,7 @@ export class MqttHassPublisher {
     this.availabilityTopic = options.mqtt.availabilityTopic;
     this.stateTopic = options.mqtt.stateTopic;
     this.changeTopic = options.mqtt.changeTopic;
+    this.payloadFile = options.hass.payloadFile;
   }
 
   public static async start(receivers: ReceiverConfig[], mqtt: MqttConfig, hass: HomeAssistantConfig) {
@@ -86,6 +90,20 @@ export class MqttHassPublisher {
     await client.endAsync();
 
     console.debug('Disconnected from MQTT');
+
+    if (hass.payloadFile) {
+      console.debug(`Writing payloads to file ${hass.payloadFile}`);
+      
+      const payloads: Array<{ topic: string, payload: string }> = [];
+
+      for (const manager of managers) {
+        for (const payload of manager.payloads) {
+          payloads.push(payload);
+        }
+      }
+
+      await fs.writeFile(hass.payloadFile, JSON.stringify(payloads));
+    }
   }
 
   public static create(options: MqttManagerOptions) {
@@ -106,18 +124,18 @@ export class MqttHassPublisher {
 
       const topic = `${this.hass.prefix}/device/${deviceId}/config`;
 
-      // await this.publishToTopic(
-      //   topic,
-      //   {
-      //     dev: {
-      //       ids: deviceId,
-      //       name: deviceName,
-      //     },
-      //     o: {
-      //       name: 'denon-mqtt-ha',
-      //     },
-      //   },
-      // );
+      await this.publishToTopic(
+        topic,
+        {
+          dev: {
+            ids: deviceId,
+            name: deviceName,
+          },
+          o: {
+            name: 'denon-mqtt-ha',
+          },
+        },
+      );
 
       const payload = {
         dev: {
@@ -166,6 +184,10 @@ export class MqttHassPublisher {
   async publishToTopic(topic: string, payload: any) {
     console.log(`[MQTT:${topic}]->${JSON.stringify(payload, null, 2)}`);
 
+    if (this.hass.payloadFile) {
+      this.payloads.push({ topic, payload: JSON.stringify(payload) })
+    }
+
     return this.client.publishAsync(topic, JSON.stringify(payload));
   }
 
@@ -179,7 +201,7 @@ export class MqttHassPublisher {
     } else if (type === 'select') {
       config.entity['options'] = this.receiver.zones[zone - 1].sources;
     } else if (config.id === 'state') {
-      config.entity['json_attributes_topic'] = `${this.mqtt.prefix}/${this.receiver.id}/${zoneId}/${this.changeTopic}`;
+      config.entity['json_attributes_topic'] = `${this.mqtt.prefix}/${this.receiver.id}/${zoneId}/${this.stateTopic}`;
     } else if (config.id === 'mute_toggle') {
       config.entity['command_template'] =
         `{ \"mute\": { \"text\": {% if is_state('switch.${this.receiver.id}_${zoneId}_mute', 'off') %}\"ON\"{% else %}\"OFF\"{% endif %} } }`;
